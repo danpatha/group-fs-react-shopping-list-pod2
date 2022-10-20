8b8faadcc09919af41527f17bce718c59b0a1306
Noelani West <noelani.rose14@gmail.com>
2022-10-20 09:43:22 -0500

    create database

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+DROP TABLE IF EXISTS "shopping";
+CREATE TABLE "shopping" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(80),
+	"quantity" INT, 
+	"unit" varchar(20)
+);
+
+INSERT INTO "shopping"
+	("name", "quantity", "unit")
+VALUES ('Potatoes', '4', '2 lbs');
