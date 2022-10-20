6505f552d10c750d6e0e27e8080844d0da9c71d8
Daniel Pathammavong <patha037@umn.edu>
2022-10-20 13:21:14 -0500

    Remove line of popup code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import ItemForm from '../ItemForm/ItemForm';
 import ItemList from '../ItemList/ItemList';
 import Header from '../Header/Header.jsx';
 import './App.css';
-import { response } from 'express';
+
 
 
 function App() {
