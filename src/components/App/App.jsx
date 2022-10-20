3de0ba5ce2baa5b2a5da5fe069dbcac532807d94
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-20 11:37:55 -0500

    html frame

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,44 @@ function App() {
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <h2>Add an Item</h2>
+                <form>
+                    <label>Item</label>
+                    <input type="text" required placeholder="Item"></input>
+
+                    <label>Quantity</label>
+                    <input type="text" required placeholder="Quantity needed"></input>
+
+                    <label>Unit</label>
+                    <input type="text" required placeholder="Unit"></input>
+                </form>
+
+                <article>
+                    <header>
+                        <h2>Shopping List</h2>
+                        <button id="resetBtn">Reset</button>
+                        <button id="clearBtn">Clear</button>
+                    </header>
+
+                    <section>
+                        <div class="listItem">
+                            <h4>Pineapple</h4>
+                            <p>5 lbs</p>
+                            <button class="purchasedBtn">Purchased</button>
+                            <button class="removeBtn">Remove</button>
+                        </div>
+                        <div class="listItem">
+                            <h4>Apple Jacks</h4>
+                            <p>3 boxes</p>
+                            <button class="purchasedBtn">Purchased</button>
+                            <button class="removeBtn">Remove</button>
+                        </div>
+                    </section>
+                </article>
+            <footer>
+                <p>Created by Pod 2 on October 20th, 2022 at Prime Digital Academy</p>
+                <p>Copywrite: 2022</p>
+            </footer>
             </main>
         </div>
     );
