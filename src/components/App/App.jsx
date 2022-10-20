6ab3d9bdd64e3859273a00096bc17a162ec7651b
Noelani West <noelani.rose14@gmail.com>
2022-10-20 11:31:24 -0500

    useEffect on load, create axios get

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import { useState, useEffect} from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+    let [itemList, setItemList] = useState([]);
+
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    const getItems = () => {
+        axios.get('/items')
+            .then(response => {
+                setItemList(response.data)
+            })
+            .catch(error => {
+                console.log('GETting itemlist failed', error);
+            })
+    }
+
+
     return (
         <div className="App">
             <Header />
